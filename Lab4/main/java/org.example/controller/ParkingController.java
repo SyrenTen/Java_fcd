3097ad2ad7c3914eb55fd19ffc829c1346165dc3
Vitaliia <86183868+SyrenTen@users.noreply.github.com>
2024-05-27 10:13:50 +0200

    Create ParkingController.java

---
+++ Lab4/main/java/org.example/controller/ParkingController.java
@@ -0,0 +1,54 @@
+package org.example.controller;
+
+import org.example.model.Car;
+import org.example.service.ParkingServiceList;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/parking")
+public class ParkingController {
+
+    private final ParkingServiceList parkingServiceList;
+
+    public ParkingController(ParkingServiceList parkingServiceList) {
+        this.parkingServiceList = parkingServiceList;
+    }
+
+    @GetMapping
+    public List<Car> getCars() {
+        return parkingServiceList.getCarsList();
+    }
+
+    @GetMapping("/cars/{carNumber}")
+    public Car getCar(@PathVariable("carNumber") String carNumber) {
+        Car car = parkingServiceList.getCar(carNumber);
+        if (car == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return car;
+    }
+
+    @PostMapping("/cars")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Car parkCar(@RequestBody Car car) {
+        if (parkingServiceList.parkCar(car)) {
+            return parkingServiceList.getCar(car.geterCarNumber());
+        } else {
+            throw new ResponseStatusException(HttpStatus.CONFLICT);
+        }
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/car/{carNumber}")
+    public void removeCar(@PathVariable("carNumber") String carNumber) {
+        Car car = parkingServiceList.getCar(carNumber);
+        if (car == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        parkingServiceList.removeCar(carNumber);
+    }
+}
